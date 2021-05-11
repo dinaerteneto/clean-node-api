36ae2e5b20c6d8d56ddc31ef7a899ca703659175
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-11 08:45:53 -0300

    refactor: group all protocols

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from '../../protocols/encrypter'
+import { Encrypter } from './adb-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
 
 interface SutTypes {
