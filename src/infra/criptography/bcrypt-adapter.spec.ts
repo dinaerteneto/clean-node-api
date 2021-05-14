8919416b3834f7f5ee18a4acb1d92428338fbf8d
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-14 17:46:37 -0300

    refactor: better code

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -28,7 +28,13 @@ describe('Bcrypt adapter', () => {
 
   test('Should throws if bcrypt throws', async () => {
     const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(bcrypt, 'hash')
+      .mockReturnValueOnce(
+        await new Promise<any>((resolve, reject) => {
+          reject(new Error())
+        })
+      )
+
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
   })
