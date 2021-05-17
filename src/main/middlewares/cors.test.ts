9bdd8c560de0aaed31c66b08046758929d803a8b
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-17 18:54:40 -0300

    feat: ensure crequests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send()
+      res.send('')
     })
     await request(app)
       .get('/test_cors')
