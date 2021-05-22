14e3a20cb8b6e308de2e8ce101c536305ba80e6c
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-22 09:12:57 -0300

    feat: ensure LoginController return 401 if invalid credations are provided

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,5 @@
 import { ServerError } from '../errors'
+import { UnauthorizedError } from '../errors/unauthorid-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
@@ -14,4 +15,9 @@ export const serverError = (error: Error): HttpResponse => ({
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
 })
