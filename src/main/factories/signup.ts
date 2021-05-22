972b59a4e4de8b5bd3b35b9b52a6b7d31d1e6725
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-22 06:52:40 -0300

    feat: add LogMongoRepostiory to a SignUpFactory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapater'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdpater } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
@@ -8,9 +9,10 @@ import { LogControllerDecorator } from '../decorators/log'
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const emailValidatorAdpater = new EmailValidatorAdpater()
-  const encrypter = new BcryptAdapter(salt)
+  const bCryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(encrypter, accountMongoRepository)
+  const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdpater, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
