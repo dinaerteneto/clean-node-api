194aaeda505efd704fa82d36bdd578f701c1084b
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-24 20:10:54 -0300

    test: ensure DbAuthentication return null if LoadAccountByEmailRepository returns null

--- src/data/usecases/ authentication/db-authencation.spec.ts
+++ src/data/usecases/ authentication/db-authencation.spec.ts
@@ -50,4 +50,10 @@ describe('DbAuthencation UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { loadAccountByEmailRepositoryStub, sut } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
