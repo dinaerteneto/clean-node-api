0a729ce4918b0e26c46de819182162e02be82538
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-24 09:59:22 -0300

    feat: add LoginValidationFactory

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,9 +1,9 @@
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapater'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { LogControllerDecorator } from '../decorators/log'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapater'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { LogControllerDecorator } from '../../decorators/log'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): SignUpController => {
