98468a7e6b70848b33c84e3028a02f4f39aff58b
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-26 09:43:08 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spect.ts
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken"
+
+describe('Jwt Adapter', () => {
+  test('Should call sign with correct values', async () => {
+      const sut = new JwtAdapter('secret')
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalled({id: 'any_id'}, 'secret')
+  })
+})
