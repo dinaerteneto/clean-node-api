872157506228590cb8308f5ff84f5bee1b2c33af
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-26 09:01:08 -0300

    test: ensure BCryptAdapter calls bcrypt compare with correct value

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,8 +4,13 @@ import { BcryptAdapter } from './bcrypt-adapater'
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return new Promise(resolve => resolve('hash'))
+  },
+  
+  async compare(): Promise<boolean> {
+    return new Promise(resolve => resolve(true))
   }
-}))
+  })
+)
 
 const salt = 12
 const makeSut = (): BcryptAdapter => {
@@ -13,30 +18,31 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt adapter', () => {
-  test('Should call bcrypt with correct values', async () => {
+  test('Should call hash with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should return a hash on success', async () => {
+  test('Should return a valid hash on hash success', async () => {
     const sut = makeSut()
     const hash = await sut.hash('any_value')
     expect(hash).toBe('hash')
   })
-  /*
+  
   test('Should throws if bcrypt throws', async () => {
     const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash')
-      .mockReturnValueOnce(
-        await new Promise<any>((resolve, reject) => {
-          reject(new Error())
-        })
-      )
-
-    const promise = sut.encrypt('any_value')
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.hash('any_value')
     await expect(promise).rejects.toThrow()
   })
-  */
+
+  test('Should call compare with correct values', async () => {
+    const sut = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+  
 })
