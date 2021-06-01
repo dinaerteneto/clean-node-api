efb489413551d39800924daecaec406292e93ec2
Dinaerte Neto <dinaerteneto@gmail.com>
2021-06-01 17:30:36 -0300

    refactor: move validation to its own layer

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -1,5 +1,6 @@
 import validator from 'validator'
 import { EmailValidator } from '../../validation/protocols/email-validator'
+
 export class EmailValidatorAdpater implements EmailValidator {
   isValid (email: string): boolean {
     return validator.isEmail(email)
