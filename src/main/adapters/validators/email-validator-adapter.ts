f3f5655543cb058dbf4aa1dcb2ea9ab48a1281da
Dinaerte Neto <dinaerteneto@gmail.com>
2021-06-01 17:40:58 -0300

    refactor: move validation to its own layer

---
+++ src/main/adapters/validators/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import validator from 'validator'
+import { EmailValidator } from '../../../validation/protocols/email-validator'
+
+export class EmailValidatorAdpater implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,45 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from '../../protocols/validation'
+import { ValidationComposite } from './validation-composite'
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationComposite(validationStubs)
+  return { validationStubs, sut }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error('field'))
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toEqual(new Error('field'))
+  })
+
+  test('Should not return if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
+})
