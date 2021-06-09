4a4fcaf0ad1e386bce9f212164214491884b78c6
Dinaerte Neto <dinaerteneto@gmail.com>
2021-06-08 23:26:24 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add surveys success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'question',
+          answers: [
+            { image: 'http://image-name.com', answer: 'answer 1' },
+            { answer: 'answer 2' }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
