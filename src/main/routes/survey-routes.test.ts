e8ff250b096b3c20f1ffe68d72ec02152ee09043
Dinaerte Neto <dinaerteneto@gmail.com>
2021-06-15 14:29:59 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('Should return 204 on add surveys success', async () => {
+    test('Should return 403 on add surveys without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -30,7 +30,7 @@ describe('Survey Routes', () => {
             { answer: 'answer 2' }
           ]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
