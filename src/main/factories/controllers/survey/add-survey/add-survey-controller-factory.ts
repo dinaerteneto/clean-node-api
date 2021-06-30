d992f592c48633ea7b09182de56ce696e01364c9
Dinaerte Neto <dinaerteneto@gmail.com>
2021-06-30 18:21:22 -0300

    feat: ensure save survey result route returns 403 if no token is provided

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeSurveyValidation } from './add-survey-validation-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
-import { makeDbAddSurvey } from '@/main/factories/usecases/survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '@/main/factories/usecases/survey/add-survey/db-add-survey-factory'
 import { AddSurveyController } from '@/presentation/controllers/survey/add-survey/add-survey-controller'
 import { Controller } from '@/presentation/protocols'
 
