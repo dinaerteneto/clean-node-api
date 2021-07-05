ca24eb135f3ea35e9e8ce4ac355faa6c60b5fde8
Dinaerte Neto <dinaerteneto@gmail.com>
2021-07-05 09:37:55 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,34 @@
+import { loginPath } from './paths/login-path'
+import { errorSchema, loginParamsSchema, accountSchema } from './schemas'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node Api',
     description: 'Api do curso do mango pra realizar enquete entre programadores',
     version: '1.0.0'
+  },
+  servers: [
+    { url: '/api' }
+  ],
+  tags: [
+    { name: 'Login' },
+    { name: 'Enquete' }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    unauthorized,
+    serverError,
+    notFound
   }
+
 }
